f79781e342efb446b9e959caebee21bda5ddc261
Mark A. Leone <mark.leone@axiosengineering.com>
2015-12-21 14:02:22 -0500

    OPtimize

--- projectEulerPy/euler/P7TenK01Prime.py
+++ projectEulerPy/euler/P7TenK01Prime.py
@@ -26,7 +26,8 @@ def primeFactors(n):
         if n/prod != 1:
             primes[n/prod] += 1 
         return primes
-    
+
+# Brute force, takes over half an hour
 # start = timeit.default_timer()
 # nPrimes = 0
 # f = 2
@@ -54,26 +55,27 @@ def primesInRange(m, n, primes):
         p = m
     while p**2 <= n:
         j = p**2
-        while j <= n:        
-            a[j] = 0
+        while j <= n:
+            if j >= m:
+                a[j] = 0
             j += p
         if primesCopy:
             p = primesCopy.pop()
         else:
             p = 3 if p == 2 else p + 2
+            while not p in a or a[p] == 0:
+                p += 2
     return [n for n in a.keys() if a[n] == 1]
 
 def nthPrime(n):
     count = 0
     start = 2
-    sieve = 10000
-#     sieve = 2 * n * int(math.log(n))
+    sieve = 100000
     primes = []
     while count < n:
         primes.extend(primesInRange(start, start + sieve, primes))
         count = len(primes)
         start += sieve
-#         print count, "primes found so far"
     if count == n:
         return primes[-1]
     else:
